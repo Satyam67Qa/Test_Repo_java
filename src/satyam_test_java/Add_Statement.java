b1eab617ebade32ac1b21a28a8e4490c32244da1
Satyam67Qa <satyamsingh28389@gmail.com>
2023-06-06 15:16:43 +0530

    New program

---
+++ src/satyam_test_java/Add_Statement.java
@@ -0,0 +1,9 @@
+package satyam_test_java;
+
+public class Add_Statement {
+    public static void main(String args[]){
+        String a="satyam";
+        String b="singh";
+        System.out.println(a  +   b);
+    }
+}
