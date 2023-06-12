fb99eed036c5833fd5920a83fb4009b8e6fa57f2
Satyam67Qa <satyamsingh28389@gmail.com>
2023-06-13 00:21:24 +0530

    loop

---
+++ src/Satyam_loop/L1.java
@@ -0,0 +1,14 @@
+package Satyam_loop;
+
+public class L1 {
+    public static void main(String[] args) {
+        int a ,b;
+        for (  a=0;a<=10;a++){
+            System.out.print(a);
+        }
+        for (b=0;b<10;b=b+2)//need to rember when need to add 2
+        {
+            System.out.println( "start" +b);
+        }
+    }
+}
