40770c5a565632905adc5438a233677a8e5d2675
Satyam67Qa <satyamsingh28389@gmail.com>
2023-06-21 15:38:52 +0530

    new 21-06-23

---
+++ src/Satyam_String/S1.java
@@ -0,0 +1,12 @@
+package Satyam_String;
+
+public class S1 {
+    public static void main(String[] args) {
+
+        String name= "Satyam Singh";
+        System.out.println(name.length());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.charAt(6));
+    }
+}
